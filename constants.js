c19ce6c47ba6f74ad4620e982b90008a59e5690f
ramoncordeiro <ramonestii@gmail.com>
2022-12-28 17:29:09 -0300

    uploading files

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9fb899a0d348fBEb760219D3045C4fF34517D04A";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
